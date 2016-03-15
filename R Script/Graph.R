c598e0b4bb3fc016ccf0b12f1952d4079ad67f39
Adam <tehehoho@hotmail.com>
2016-03-15 09:47:17 -0400

    R Script updated to display points and centroids

--- R Script/Graph.R
+++ R Script/Graph.R
@@ -1,12 +1,24 @@
-data <- read.csv("../Output/Classification-813202513504082", sep="",header=FALSE)
+data <- read.csv("../Output/Classification-814698944939384.txt", sep="",header=FALSE)
 x <- data[1]
 y <- data[2]
 
+data2 <- read.csv("../Output/Centroids-814698944939384.txt", sep="",header=FALSE)
+x2 <- data2[1]
+y2 <- data2[2]
 
 attach(data)
 names(getwd())
 names(x)
 
-png("Graphs/graph.png")
-matplot(x, y, pch=20, col=34, bg=34)
+png(file=paste("/Graphs/graph.png", ".png", sep=""), width=800, height=800)
+palette(rainbow(17))
+par=(bg="whitesmoke")
+plot(data$V1, data$V2, pch=20, ps=1, col=data$V3, )
+palette("default")
+points(data2$V1, data2$V2, pch=15, col=2)
+points(data2$V1, data2$V2, pch=7, col=1)
+
+#matplot(x, y, pch=20, col=34, bg=34)
+#matplot(x2, y2, pch=1, col=10, bg=10)
+
 #plot(data[0])
